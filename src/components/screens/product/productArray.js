2e289abd7f59883352c3d82b4c51386f76388936
SANJEEV KUMAR <59915362+sk749326@users.noreply.github.com>
2023-06-03 15:07:42 +0530

    git add

--- src/components/screens/product/productArray.js
+++ src/components/screens/product/productArray.js
@@ -1,3 +1,8 @@
+import image1 from "../../images/image1.jpg"
+import image2 from "../../images/image2.jpg"
+import image3 from "../../images/image3.jpg"
+import image4 from "../../images/image4.jpg"
+
 export const productList=[
     {
         productTitle:"ProductName1",
@@ -6,17 +11,17 @@ export const productList=[
     },
     {
         productTitle:"ProductName2",
-        imageUrl:"https://images.unsplash.com/photo-1677748343334-5af9f891a85e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+        imageUrl:image1,
         desc:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem ipsum laudantium qui ad porro, hic sunt id"
     },
     {
         productTitle:"ProductName3",
-        imageUrl:"https://images.unsplash.com/photo-1677748343334-5af9f891a85e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+        imageUrl:image3,
         desc:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem ipsum laudantium qui ad porro, hic sunt id"
     },
     {
         productTitle:"ProductName3",
-        imageUrl:"https://images.unsplash.com/photo-1677748343334-5af9f891a85e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+        imageUrl:image4,
         desc:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem ipsum laudantium qui ad porro, hic sunt id"
     },
 ]
