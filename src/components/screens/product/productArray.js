b312de24ced23bb7c721916e76c24c2fb5661105
SANJEEV KUMAR <59915362+sk749326@users.noreply.github.com>
2023-06-03 15:19:47 +0530

    image link update

--- src/components/screens/product/productArray.js
+++ src/components/screens/product/productArray.js
@@ -2,11 +2,12 @@ import image1 from "../../images/image1.jpg"
 import image2 from "../../images/image2.jpg"
 import image3 from "../../images/image3.jpg"
 import image4 from "../../images/image4.jpg"
+import image5 from "../../images/image6.avif"
 
 export const productList=[
     {
         productTitle:"ProductName1",
-        imageUrl:"https://images.unsplash.com/photo-1677748343334-5af9f891a85e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+        imageUrl:image5,
         desc:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem ipsum laudantium qui ad porro, hic sunt id"
     },
     {
